3da95906233147ee5688a41b0e66a7ad6289a90b
Kevin Rodriguez <Kevin Rodriguez>
2017-06-19 20:36:19 -0600

    Settings

---
+++ Mantenimientocarros/Models/Carros.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mantenimientocarros.Models
+{
+    public class Carros
+    {
+
+
+
+
+        //// Make, Model, Year and Cylinders.
+
+
+
+
+
+        public int id { get; set; }
+        public String  Model { get; set; }
+        public int Year { get; set; }
+        public int Cylinders { get; set; }
+
+    }
+
+
+}
---
+++ Mantenimientocarros/Models/CarrosController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mantenimientocarros.Models
+{
+    public class CarrosController : Controller
+    {
+        private MantenimientocarrosContext db = new MantenimientocarrosContext();
+
+        // GET: Carros
+        public ActionResult Index()
+        {
+            return View(db.Carros.ToList());
+        }
+
+        // GET: Carros/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Carros carros = db.Carros.Find(id);
+            if (carros == null)
+            {
+                return HttpNotFound();
+            }
+            return View(carros);
+        }
+
+        // GET: Carros/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Carros/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,Model,Year,Cylinders")] Carros carros)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Carros.Add(carros);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(carros);
+        }
+
+        // GET: Carros/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Carros carros = db.Carros.Find(id);
+            if (carros == null)
+            {
+                return HttpNotFound();
+            }
+            return View(carros);
+        }
+
+        // POST: Carros/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,Model,Year,Cylinders")] Carros carros)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(carros).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(carros);
+        }
+
+        // GET: Carros/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Carros carros = db.Carros.Find(id);
+            if (carros == null)
+            {
+                return HttpNotFound();
+            }
+            return View(carros);
+        }
+
+        // POST: Carros/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Carros carros = db.Carros.Find(id);
+            db.Carros.Remove(carros);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
